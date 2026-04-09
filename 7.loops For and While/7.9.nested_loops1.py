00251b28659c5539404f9adf782673b82fa7b13c
nata-mhv <nata.mkhv@gmail.com>
2026-04-09 20:07:47 +0200

    New tasks of 7.10 added

--- 7.loops For and While/7.9.nested_loops1.py
+++ 7.loops For and While/7.9.nested_loops1.py
@@ -35,8 +35,8 @@ for i in range(1, n+1):
 # 4444
 # 55555
 # ...
-n = int(input()) #5
-for i in range(1, n+1): #1,2,3,4,5
+n = int(input())
+for i in range(1, n+1):
     for j in range(1, i+1):
         print(i, end='')
     print()
