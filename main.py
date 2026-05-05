bb61a493e297728df05bd5f2bdab4940565478df
nata-mhv <nata.mkhv@gmail.com>
2026-05-05 16:18:57 +0200

    11.1 is solved

--- main.py
+++ main.py
@@ -14,11 +14,3 @@ from math import *
 
 #print("Максимальная цифра равна", max_digit)
 #print("Минимальная цифра равна", min_digit)
-s = input()
-new_s = ''
-for i in range(len(s)):
-    selection = s[s.find('h')+1:s.rfind('h')]
-    conversion = selection[::-1]
-    new_s = s[:s.find('h')+1] + conversion + s[s.rfind('h'):]
-
-print(new_s)
